139e92afd871dafe0a839fcc90a67c436955baf4
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-05-19 19:14:28 +0900

    feat:chrome local storage 에 검색 기록 저장

---
+++ src/saveHistory.ts
@@ -0,0 +1,45 @@
+type HistoryItem = Partial<{
+  searchUrl: string;
+  searchQuery: string;
+  visitTime: number;
+}>;
+
+interface StorageData {
+  savedHistory: HistoryItem[];
+}
+
+const isUrlDuplicated = (
+  searchUrl: string,
+  savedHistory: HistoryItem[]
+): boolean => {
+  return savedHistory.some((item) => item.searchUrl === searchUrl);
+};
+
+export function saveHistoryFromSearch(
+  searchUrl: string,
+  searchQuery: string
+): void {
+  // 현재 시간 저장
+  const searchTime = new Date().getTime();
+
+  chrome.storage.local.get<StorageData>("savedHistory", (histories) => {
+    let savedHistory = histories.savedHistory || [];
+
+    // 검색 URL이 이미 저장되어 있는지 확인
+    if (isUrlDuplicated(searchUrl, savedHistory)) {
+      return;
+    }
+
+    // 새 검색 URL 추가
+    savedHistory.push({
+      searchUrl,
+      searchQuery,
+      visitTime: searchTime,
+    });
+
+    // 업데이트된 기록 저장
+    chrome.storage.local.set({ savedHistory }, () => {
+      console.log("새 검색 기록이 저장되었습니다:", savedHistory);
+    });
+  });
+}
