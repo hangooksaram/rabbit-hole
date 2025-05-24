c7776f8ed3413b03893a421a83d6190c107cde86
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-05-24 17:05:48 +0900

    refactor:Popup 클래스에서 dom 조작하는 멤버, 메서드 PopupElements 클래스로 분리

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -1,77 +1,59 @@
-import { History, RabbitHole } from "../chromeApi/chromeLocalData";
 import ChromeStorage from "../chromeApi/storageData";
 import { initRabbitHole } from "../rabbitHole/rabbltHole";
+import PopupElements from "./popupElements";
 
 class Popup {
-  private searchStatusElement: HTMLElement | null = null;
-  private startButton: HTMLButtonElement | null = null;
-  private statusText: HTMLElement | null = null;
-  private rabbitHoleHistoryElement: HTMLUListElement | null = null;
-  private rabbitHoleDepthElement: HTMLSpanElement | null = null;
-  private rabbitHole: RabbitHole | null = null;
-  private recentSearch: History | null = null;
+  private popupElements!: PopupElements;
 
   constructor() {
+    this.popupElements = new PopupElements();
     this.initPopup();
   }
 
-  private getPopupElements() {
-    this.searchStatusElement = document.getElementById("searchStatus");
-    this.startButton = document.getElementById(
-      "startButton"
-    ) as HTMLButtonElement;
-    this.statusText = document.getElementById("statusText");
-    this.rabbitHoleHistoryElement = document.getElementById(
-      "rabbitHoleHistory"
-    ) as HTMLUListElement;
-
-    this.rabbitHoleDepthElement = document.getElementById(
-      "rabbitHoleDepth"
-    ) as HTMLSpanElement;
-  }
-
-  private async setSearchStatusElement() {
+  private async setRecentSearch() {
     const recentSearch = await ChromeStorage.get("recentSearch");
 
     if (!recentSearch) {
-      if (this.searchStatusElement) {
-        this.searchStatusElement.innerHTML =
-          "검색 페이지에서 검색을 먼저 수행해주세요.";
-      }
+      this.popupElements.setRecentSearchElement(
+        "검색 페이지에서 검색을 먼저 수행해주세요."
+      );
       return;
-    } else {
-      if (this.searchStatusElement) {
-        this.searchStatusElement.innerHTML = recentSearch.searchQuery!;
-      }
     }
+    this.popupElements.setRecentSearchElement(recentSearch.searchQuery!);
   }
 
-  private async setRabbitHoleElement() {
+  private async setRabbitHole() {
     const rabbitHole = await ChromeStorage.get("rabbitHole");
 
-    rabbitHole.history.map((item) => {
-      const historyElement = document.createElement("div");
+    rabbitHole.history.forEach((item) => {
+      const historyElement = document.createElement("li");
       historyElement.innerHTML = item.searchQuery!;
-      this.rabbitHoleHistoryElement?.appendChild(historyElement);
-      this.rabbitHoleDepthElement!.innerHTML = rabbitHole.holeDepth!.toString();
+      this.popupElements.appendRabbitHoleHistoryElement(historyElement);
+      this.popupElements.setRabbitHoleDepthElement(rabbitHole.holeDepth!);
+    });
+  }
+
+  private async setNewRabbitHole() {
+    const recentSearch = await ChromeStorage.get("recentSearch");
+
+    initRabbitHole(recentSearch?.searchQuery || "", () => {
+      this.popupElements.setStatusTextElement(
+        "새로운 Rabbit Hole이 저장되었습니다."
+      );
+
+      setTimeout(() => {
+        this.popupElements.setStatusTextElement("");
+      }, 1000);
     });
   }
 
   private initPopup() {
-    this.getPopupElements();
+    this.popupElements.addStartButtonClickListener(() =>
+      this.setNewRabbitHole()
+    );
+    this.setRabbitHole();
 
-    this.startButton?.addEventListener("click", () => {
-      initRabbitHole(this.recentSearch?.searchQuery || "", () => {
-        this.statusText!.innerHTML = "새로운 Rabbit Hole이 저장되었습니다.";
-
-        setTimeout(() => {
-          this.statusText!.innerHTML = "";
-        }, 1000);
-      });
-    });
-    this.setRabbitHoleElement();
-
-    this.setSearchStatusElement();
+    this.setRecentSearch();
   }
 }
 
---
+++ src/popup/popupElements.ts
@@ -0,0 +1,66 @@
+class PopupElements {
+  private recentSearchElement!: HTMLElement;
+  private startButtonElement!: HTMLButtonElement;
+  private statusTextElement!: HTMLElement;
+  private rabbitHoleHistoryElement!: HTMLUListElement;
+  private rabbitHoleDepthElement!: HTMLSpanElement;
+
+  constructor() {
+    try {
+      this.init();
+    } catch (error) {
+      console.error("Initialization failed:", error);
+    }
+  }
+
+  private init() {
+    this.recentSearchElement = document.getElementById(
+      "searchStatus"
+    ) as HTMLElement;
+    this.startButtonElement = document.getElementById(
+      "startButton"
+    ) as HTMLButtonElement;
+    this.statusTextElement = document.getElementById(
+      "statusText"
+    ) as HTMLElement;
+    this.rabbitHoleHistoryElement = document.getElementById(
+      "rabbitHoleHistory"
+    ) as HTMLUListElement;
+
+    this.rabbitHoleDepthElement = document.getElementById(
+      "rabbitHoleDepth"
+    ) as HTMLSpanElement;
+  }
+
+  public setRecentSearchElement(value: string) {
+    this.recentSearchElement.innerHTML = value;
+  }
+
+  public setStartButtonElement(value: string) {
+    this.startButtonElement!.innerHTML = value;
+  }
+
+  public setStatusTextElement(value: string) {
+    this.statusTextElement!.innerHTML = value;
+  }
+
+  public appendRabbitHoleHistoryElement(value: HTMLLIElement) {
+    this.rabbitHoleHistoryElement?.appendChild(value);
+  }
+
+  public setRabbitHoleHistoryElement(value: string) {
+    const historyElement = document.createElement("li");
+    historyElement.innerHTML = value;
+    this.rabbitHoleHistoryElement!.appendChild(historyElement);
+  }
+
+  public setRabbitHoleDepthElement(value: number) {
+    this.rabbitHoleDepthElement!.innerHTML = value.toString();
+  }
+
+  public addStartButtonClickListener(callback: () => void) {
+    this.startButtonElement!.addEventListener("click", callback);
+  }
+}
+
+export default PopupElements;
