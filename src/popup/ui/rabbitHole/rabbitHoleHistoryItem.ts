b50952d1d87f4a80d3eafd4bfda54852dab9e0cb
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-06-08 16:36:19 +0900

    feat:검색 시간 history 에 추가

--- src/popup/ui/rabbitHole/rabbitHoleHistoryItem.ts
+++ src/popup/ui/rabbitHole/rabbitHoleHistoryItem.ts
@@ -7,13 +7,11 @@ const defaultLogoImgPath = "/default-search-engine-logo.png";
 
 export class RabbitHoleHistoryItem {
   private element: HTMLLIElement = document.createElement("li");
-  private imageElement: HTMLImageElement = document.createElement("img");
   private searchHistory: History;
 
   constructor(history: History) {
     this.searchHistory = history;
 
-    this.setRabbitHoleHistoryItemLogoUI();
     this.setRabbitHoleHistoryItemUI();
   }
 
@@ -34,23 +32,52 @@ export class RabbitHoleHistoryItem {
     return this.element;
   }
 
-  setSearchQueryText() {
-    this.element.innerText = this.searchHistory.searchQuery!;
+  createRabbitHoleSearchTimeElement() {
+    const timeElement = document.createElement("span");
+    timeElement.innerText = new Date(
+      this.searchHistory.visitTime!
+    ).toLocaleString();
+    return timeElement;
   }
 
-  setRabbitHoleHistoryItemLogoUI() {
+  createRabbitHoleHistoryItemLogoElement() {
+    const imgElement = document.createElement("img");
     const imgSrc = this.getRabbitHoleHistoryIconPath();
 
-    this.imageElement.width = 20;
-    this.imageElement.height = 20;
-    this.imageElement.alt = "Search Engine Icon";
+    imgElement.width = 20;
+    imgElement.height = 20;
+    imgElement.alt = "Search Engine Icon";
 
-    this.imageElement.src = imgSrc;
+    imgElement.src = imgSrc;
+
+    return imgElement;
+  }
+
+  createSearchQueryElement() {
+    const searchQueryElement = document.createElement("div");
+    searchQueryElement.innerText = this.searchHistory.searchQuery!;
+    return searchQueryElement;
+  }
+
+  createRabbitHoleInformationelement() {
+    const infoElement = document.createElement("div");
+    const searchQueryElement = this.createSearchQueryElement();
+    const timeElement = this.createRabbitHoleSearchTimeElement();
+
+    infoElement.appendChild(searchQueryElement);
+    infoElement.appendChild(timeElement);
+    infoElement.classList.add("rabbit-hole-history-item-info");
+
+    return infoElement;
   }
 
   setRabbitHoleHistoryItemUI() {
-    this.setSearchQueryText();
-    this.element.prepend(this.imageElement);
+    const imgElement = this.createRabbitHoleHistoryItemLogoElement();
+    const infoElement = this.createRabbitHoleInformationelement();
+
+    this.element.prepend(imgElement);
+    this.element.appendChild(infoElement);
+
     this.element.classList.add("button", "rabbit-hole-history-item");
   }
 }
