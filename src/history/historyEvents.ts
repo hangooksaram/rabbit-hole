1e0efe678854a0019d2ccc9b861b3f87e1f9cd0f
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-07-10 22:22:51 +0900

    ui:Improve history menu layout and initialization

--- src/history/historyEvents.ts
+++ src/history/historyEvents.ts
@@ -1,10 +1,12 @@
 import History from "./history";
+import HistoryController from "./historyController";
 import HistoryElements from "./ui/historyElements";
 
 class HistoryEvents {
   static addHistoryOpenAndCloseButtonEvent() {
-    HistoryElements.historyOpenButton.addEvent("click", () => {
+    HistoryElements.historyOpenButton.addEvent("click", async () => {
       History.UI.toggleHistory();
+      await History.Controller.initHistoryList();
     });
 
     HistoryElements.historyCloseButton.addEvent("click", () => {
